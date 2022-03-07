8db0409637287c5aa80e40a7b71a7b543ac1875b
Phandal <bfeld.111@gmail.com>
2022-03-06 19:52:07 -0600

    Added rules for some windows

--- config.h
+++ config.h
@@ -61,6 +61,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "discord",  NULL,       NULL,       1 << 3,       0,            0 },
 	//{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, 
 };
 

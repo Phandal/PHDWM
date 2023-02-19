1f9283b35333e4aff4f19c6ec8f2d1feccbbb205
Phandal <bfeld.111@gmail.com>
2023-02-19 10:12:37 -0600

    vanity-gaps without keybinds

--- config.def.h
+++ config.def.h
@@ -5,6 +5,11 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edges */
+static const unsigned int gappov    = 10;       /* horiz outer gap between windows and screen edges */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"JetBrainsMono Nerd Font Mono:pixelsize=8:antialias=true:autohint=true"};

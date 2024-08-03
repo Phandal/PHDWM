d9717031a2c5996021a5c9e1202cb407dacb752f
Phandal <bfeld.111@gmail.com>
2024-08-02 21:29:32 -0500

    everforest theme

--- config.def.h
+++ config.def.h
@@ -29,14 +29,30 @@ static const char dmenufont[] =
 // static const char col_gray3[] = "#E5E9F0";
 // static const char col_gray4[] = "#4C566A";
 // static const char col_cyan[] = "#8FBCBB";
+// static const char *colors[][3] = {
+//     /*               fg         bg         border   */
+//     [SchemeNorm] = {col_gray3, col_gray1, col_gray1},
+//     [SchemeSel] = {col_gray3, col_gray4, col_gray4},
+// };
 
 // #### GRUVBOX ####
-static const char col_gray1[] = "#282828";
-static const char col_gray2[] = "#504945";
-static const char col_gray3[] = "#EBDBB2";
-static const char col_gray4[] = "#504945";
-static const char col_cyan[] = "#B8BB26";
+// static const char col_gray1[] = "#282828";
+// static const char col_gray2[] = "#504945";
+// static const char col_gray3[] = "#EBDBB2";
+// static const char col_gray4[] = "#504945";
+// static const char col_cyan[] = "#B8BB26";
+// static const char *colors[][3] = {
+//     /*               fg         bg         border   */
+//     [SchemeNorm] = {col_gray3, col_gray1, col_gray1},
+//     [SchemeSel] = {col_gray3, col_gray4, col_gray4},
+// };
 
+// #### Everforest ####
+static const char col_gray1[] = "#2D353B";
+static const char col_gray2[] = "#343F44";
+static const char col_gray3[] = "#D3C6AA";
+static const char col_gray4[] = "#3D484D";
+static const char col_cyan[] = "#A7C080";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray1},

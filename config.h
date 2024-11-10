ce662b40a46c6ba89a70775da4e58edaa4206422
Phandal <bfeld.111@gmail.com>
2024-11-10 13:05:52 -0600

    changed to brave browser

--- config.h
+++ config.h
@@ -115,7 +115,7 @@ static const Layout layouts[] = {
 static const char *dmenucmd[] = {"dmenu_run", "-fn", dmenufont, "-nb",
                                  col_gray1,   "-nf", col_gray3, "-sb",
                                  col_gray2,   "-sf", col_gray3, NULL};
-static const char *termcmd[] = {"st", NULL};
+static const char *termcmd[] = {"alacritty", NULL};
 /*static const char *browsercmd[] = {"google-chrome-stable", NULL};*/
 static const char *browsercmd[] = {"brave", NULL};
 static const char *editorcmd[] = {"emacsclient", "-n", "-c", NULL};
